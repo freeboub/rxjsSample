975f8c40531f2a052ef0a98b720da3b8b2e6c24d
olivier <olivier.bouillet@ifeelsmart.com>
2023-02-24 09:41:56 +0100

    fix: first sample version

---
+++ App.tsx
@@ -0,0 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React, {useCallback, useEffect, useState} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import {ajax} from 'rxjs/ajax';
+
+function Elt(): JSX.Element {
+  const [loopIdx, setLoopIdx] = useState(0);
+
+  const test = useCallback(() => {
+    console.log('call ajax loop', loopIdx);
+    return ajax({
+      url: 'https://youtube.com/',
+      method: 'GET',
+    }).subscribe(
+      () => {
+        console.log('result');
+        setLoopIdx(loopIdx + 1);
+      },
+      _error => {
+        setLoopIdx(loopIdx + 1);
+        console.log('error');
+      },
+    );
+  }, [loopIdx]);
+
+  useEffect(() => {
+    test();
+  }, [test]);
+
+  return <></>;
+}
+
+function App(): JSX.Element {
+  const [showElt, setShowElt] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowElt(false);
+    }, 60000);
+  }, []);
+
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: Colors.darker,
+      }}>
+      {showElt ? <Elt /> : null}
+
+      <Text style={{color: 'white'}}>Hello world</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
