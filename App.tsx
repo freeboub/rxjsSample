a981113f24a8ff9cf157a87c051893e55a645a22
olivier <olivier.bouillet@ifeelsmart.com>
2023-03-06 08:55:24 +0100

    fix: add buttons to stop ajax loop and to trigger manually gc()
    also remove intermediate Elt added for testing

--- App.tsx
+++ App.tsx
@@ -6,14 +6,23 @@
  */
 
 import React, {useCallback, useEffect, useState} from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, TouchableOpacity} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import {ajax} from 'rxjs/ajax';
 
-function Elt(): JSX.Element {
+function App(): JSX.Element {
+  const [loopStarted, setLoopStarted] = useState(true);
   const [loopIdx, setLoopIdx] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      console.log('calling gc');
+      // FIXME should play with this timing
+      // gc();
+    }, 60000);
+  }, []);
 
   const test = useCallback(() => {
     console.log('call ajax loop', loopIdx);
@@ -25,7 +34,8 @@ function Elt(): JSX.Element {
       next: () => {
         console.log('result');
         setLoopIdx(loopIdx + 1);
-        gc();
+        // too much agressive
+        // gc();
       },
       error: _error => {
         setLoopIdx(loopIdx + 1);
@@ -35,29 +45,39 @@ function Elt(): JSX.Element {
   }, [loopIdx]);
 
   useEffect(() => {
-    test();
-  }, [test]);
+    if (loopStarted) {
+      // adding timeout doesn't help
+      //setTimeout(() => {
+      test();
+      //}, 1);
+    }
+  }, [test, loopStarted, loopIdx]);
 
-  return <></>;
-}
-
-function App(): JSX.Element {
-  const [showElt, setShowElt] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setShowElt(false);
-    }, 60000);
-  }, []);
-
+  const testStyle = {color: 'white'};
   return (
     <SafeAreaView
       style={{
         backgroundColor: Colors.darker,
       }}>
-      {showElt ? <Elt /> : null}
-
-      <Text style={{color: 'white'}}>Hello world</Text>
+      <Text style={testStyle}>Hello world</Text>
+      <TouchableOpacity
+        onPress={() => {
+          gc();
+        }}>
+        <Text style={testStyle}>LAUNCH GC</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          setLoopStarted(false);
+        }}>
+        <Text style={testStyle}>STOP LOOP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          setLoopStarted(true);
+        }}>
+        <Text style={testStyle}>START LOOP</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
